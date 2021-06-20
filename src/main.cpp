6e69f54dbd592591300a0e35ec90ae90042d0d84
phplego <phplego@gmail.com>
2021-06-20 22:51:27 +0300

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include <RCSwitch.h>
-#include <SoftwareSerial.h>
 
-//SoftwareSerial mySerial(D8, D7); // RX, TX
 
 RCSwitch mySwitch = RCSwitch();
 
@@ -11,8 +9,7 @@ long count = 0;
 void setup() 
 {
   Serial.begin(74880);
-  //mySerial.begin(2400);
-  //pinMode(D8, INPUT);
+  //pinMode(D3, INPUT);
   pinMode(D5, OUTPUT);
   pinMode(D6, OUTPUT);
   mySwitch.enableReceive(D3);  // Receiver on interrupt 0 => that is pin #2
@@ -26,15 +23,10 @@ void loop()
   //   Serial.println(count);
   // }
 
-  // if(mySerial.available()){
-  //   int c = mySerial.read();
-  //   Serial.print(c);
-  //   Serial.print(" ");
-  // }
 
   if (mySwitch.available()) {
-    tone(D5, 800, 50);
-    tone(D6, 800, 50);
+    tone(D5, 800, 50); // beep sound
+    tone(D6, 800, 50); // flash led
     Serial.print("Value: ");
     Serial.print(mySwitch.getReceivedValue());
     Serial.print(" bit length: ");
