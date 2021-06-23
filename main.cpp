e896614be342d007a3c2a711715362fc85795edb
phplego <phplego@gmail.com>
2021-06-23 12:28:43 +0300

    publish received events to mqtt

--- main.cpp
+++ main.cpp
@@ -1,43 +1,170 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
+#include <FS.h>
 #include <RCSwitch.h>
+#include <WiFiManager.h>
+#include <MQTT.h>
+#include <ArduinoJson.h>
+
+#define RECEIVER_PIN  D5
+#define BUZZER_PIN    D6
+#define LED_PIN       D7
+
+#define MQTT_HOST  "192.168.1.157"  // MQTT host (m21.cloudmqtt.com)
+#define MQTT_PORT  11883            // MQTT port (18076)   
+#define MQTT_USER  "KJsdfyUYADSFhjscxv678"   // Ingored if brocker allows guest connection
+#define MQTT_PASS  "d6823645823746dcfgwed"   // Ingored if brocker allows guest connection
+
+WiFiManager wifiManager;
+WiFiClient  wifiClient;
+RCSwitch    mySwitch = RCSwitch();
+MQTTClient  mqttClient(10000);
 
 
-RCSwitch mySwitch = RCSwitch();
+void myTone(int freq, int duration)
+{
+    tone(BUZZER_PIN, freq, duration);
+    delay(duration);
+}
 
-long count = 0;
+
+
+void messageReceived(String &topic, String &payload) {
+    Serial.println("incoming: " + topic + " - " + payload);
+
+    DynamicJsonDocument doc(10000);
+
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error)
+        return;
+
+    // Fetch values.
+
+    if(doc.containsKey("melody")){
+        String melody = doc["melody"];
+
+        // Print values.
+        Serial.print("Playing melody: ");
+        Serial.println(melody);
+
+        //rtttl.play(melody);
+    }
+}
+
+void mqtt_connect() {
+    Serial.print("checking wifi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(1000);
+    }
+
+    Serial.print("\nconnecting...");
+    while (!mqttClient.connect("433receiver", MQTT_USER, MQTT_PASS)) {
+        Serial.print(".");
+        delay(1000);
+    }
+
+    Serial.println("\nconnected!");
+
+    mqttClient.subscribe("wifi2mqtt/433receiver/set");
+}
+
 
 void setup() 
 {
   Serial.begin(74880);
   //pinMode(D3, INPUT);
-  pinMode(D5, OUTPUT);
-  pinMode(D6, OUTPUT);
-  mySwitch.enableReceive(D3);  // Receiver on interrupt 0 => that is pin #2
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  mySwitch.enableReceive(RECEIVER_PIN);  // Receiver on interrupt 0 => that is pin #2
+
+  // Play 'start' melody
+  myTone(800, 100);
+  myTone(400, 100);
+  myTone(1200, 100);
+
+
+  String deviceName = "433receiver";
+  WiFi.hostname(deviceName);
+  WiFi.mode(WIFI_STA); // no access point after connect
+
+  wifi_set_sleep_type(NONE_SLEEP_T); // prevent wifi sleep (stronger connection)
+
+  // On Access Point started (not called if wifi is configured)
+  wifiManager.setAPCallback([](WiFiManager* mgr){
+      Serial.println( String("Please connect to Wi-Fi"));
+      Serial.println( String("Network: ") + mgr->getConfigPortalSSID());
+      Serial.println( String("Password: 12341234"));
+      Serial.println( String("Then go to ip: 10.0.1.1"));
+  });
+
+  wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  wifiManager.setConfigPortalTimeout(60);
+  wifiManager.autoConnect(deviceName.c_str(), "12341234"); // IMPORTANT! Blocks execution. Waits until connected
+
+  // Restart if not connected
+  if (WiFi.status() != WL_CONNECTED) 
+  {
+      ESP.restart();
+  }
+
+
+  mqttClient.begin(MQTT_HOST, MQTT_PORT, wifiClient);
+  mqttClient.onMessage(messageReceived);
+
+  mqtt_connect();
+
+  bool ok = mqttClient.publish("wifi2mqtt/433receiver", "started");
+  Serial.println(ok ? "Published: OK" : "Published: ERR");
+
+  // Initialize OTA (firmware updates via WiFi)
+  ArduinoOTA.begin();
+
+  // Play 'Setup completed' melody
+  myTone(1000, 100);
+  myTone(500, 100);
+  myTone(1500, 100);
+
 }
 
 void loop() 
 {
-  count ++;
-
-  // if(count % 100000 == 0){
-  //   Serial.println(count);
-  // }
-
-
   if (mySwitch.available()) {
-    tone(D5, 800, 50); // beep sound
-    tone(D6, 800, 50); // flash led
-    Serial.print("Value: ");
-    Serial.print(mySwitch.getReceivedValue());
-    Serial.print(" bit length: ");
-    Serial.print(mySwitch.getReceivedBitlength());
-    Serial.print(" Delay: ");
-    Serial.print(mySwitch.getReceivedDelay());
+    tone(BUZZER_PIN, 800, 50); // beep sound
+    tone(LED_PIN, 800, 50); // flash led
+    long receivedValue = mySwitch.getReceivedValue();
+    int delay = mySwitch.getReceivedDelay();
+
+    //Serial.print("Value: ");
+    // Serial.print(receivedValue);
+    // Serial.print(" bit length: ");
+    // Serial.print(mySwitch.getReceivedBitlength());
+    // Serial.print(" Delay: ");
+    // Serial.print(mySwitch.getReceivedDelay());
     // Serial.print("RawData: ");
     // Serial.println(mySwitch.getReceivedRawdata());
-    Serial.print(" Protocol: ");
-    Serial.println(mySwitch.getReceivedProtocol());
+    // Serial.print(" Protocol: ");
+    // Serial.println(mySwitch.getReceivedProtocol());
     
     mySwitch.resetAvailable();
+
+    DynamicJsonDocument doc(1024);
+
+    doc["id"] = receivedValue;
+    doc["delay"]   = delay;
+
+    String json;
+    serializeJson(doc, json);
+
+    mqttClient.publish("wifi2mqtt/433receiver", json);
+
   }
+
+  ArduinoOTA.handle();
+  mqttClient.loop();
+
+  if (!mqttClient.connected()) {
+    mqtt_connect();
+  }
+
 }
